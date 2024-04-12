05da11e79b1ae7e9c522dbe06ea1e1b69a29a333
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-12 12:24:46 -0400

    create-BudgetTracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { useMemo } from "react"
 import BudgetForm from "./Components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from "./Components/BudgetTracker"
+import ExpenseModal from "./Components/ExpenseModal"
 
 function App() {
- const {state, dispatch } = useBudget()
- console.log(state)
+ const {state} = useBudget()
+ const isValidBudget =  useMemo(() =>state.budget > 0, [state.budget])
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
         <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de Gastos</h1>
       </header>
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker/>: <BudgetForm />}
       </div>
+      {isValidBudget && (
+        <main className="max-x-3xl mx-auto py-10">
+          <ExpenseModal/>
+        </main>
+      )}
     </>
   )
 }
