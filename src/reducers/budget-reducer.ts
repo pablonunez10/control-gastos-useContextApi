bf8138c5eab5ca922a0564d53d457d536afe95b6
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-15 21:22:40 -0400

    add-BudgetActions

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,7 +1,24 @@
-export type BudgetActions = {type: 'add-budget', playload : {budget: number}}
-export type BudgetState = {budget: number}
+import {v4 as uuidv4 } from 'uuid'
+import { DraftExpense, Expense } from "../types"
+
+export type BudgetActions = 
+    {type: 'add-budget', playload : {budget: number}} | 
+    {type: 'show-modal'} |
+    {type: 'close-modal'} |
+    {type: 'add-expense', payload: {expense: DraftExpense}}
+
+export type BudgetState = 
+    {budget: number, modal: boolean, expenses: Expense[]}
 export const initialState : BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false,
+    expenses: []
+}
+const createExpense = (draftExpense: DraftExpense) : Expense => {
+    return {
+        ...draftExpense,
+        id: uuidv4()
+    }
 }
 export const budgetReducer = (
     state: BudgetState = initialState,
@@ -13,5 +30,32 @@ export const budgetReducer = (
             budget: action.playload.budget
         }
     }
+    if(action.type === 'show-modal') {
+        return {
+            ...state,
+            modal:true
+        }
+    }
+    if(action.type === 'close-modal') {
+        return {
+            ...state,
+            modal:false
+        }
+    }
+    if(action.type === 'add-expense') {
+        const expense = createExpense(action.payload.expense)
+        return {
+            ...state,
+            expenses: [...state.expenses, expense],
+            modal: false
+        }
+    }
+
+    // if(action.type === 'toggle-modal'){
+    //     return {
+    //         ...state,
+    //         modal: action.payload
+    //     }
+    // }
     return state
 }
