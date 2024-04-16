c940524eaf763478189e51d6329cb66031f7333a
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-16 11:44:16 -0400

    totalExpenses-remainingBudget

--- src/context/BudgetConext.tsx
+++ src/context/BudgetConext.tsx
@@ -1,27 +1,28 @@
 import { Dispatch, ReactNode, createContext, useReducer } from "react"
 import { BudgetActions, BudgetState, budgetReducer, initialState } from "../reducers/budget-reducer"
+import { useMemo } from "react"
 type BudgetContextProps = {
     state: BudgetState
     dispatch: Dispatch<BudgetActions>
-    helloworld: () => void 
+    totalExpenses: number
+    remainingBudget: number
 }
 type BudgetProviderProps = {
     children: ReactNode
 }
 export const BudgetContext = createContext<BudgetContextProps>(null!)
 export const BudgetProvider = ({children} : BudgetProviderProps) => {
-
-    const [state, dispatch ] = useReducer(budgetReducer, initialState)
-    const helloWorld = () => {
-        console.log("helloworld")
-    }
+const [state, dispatch ] = useReducer(budgetReducer, initialState)
+const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount +total, 0) ,[state.expenses])
+const remainingBudget = state.budget - totalExpenses
 
     return (
         <BudgetContext.Provider
             value={{
                 state,
                 dispatch,
-                helloworld: helloWorld
+                totalExpenses,
+                remainingBudget
             }}
         >
             {children}
