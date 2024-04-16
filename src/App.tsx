11d6cc066e62d42a8069e69ee245b2c4fdb410d3
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-16 11:42:13 -0400

    FilterByCategory

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import BudgetForm from "./Components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./Components/BudgetTracker"
 import ExpenseModal from "./Components/ExpenseModal"
 import ExpenseList from "./Components/ExpenseList"
+import FilterByCategory from "./Components/FilterByCategory"
 function App() {
  const {state} = useBudget()
  const isValidBudget =  useMemo(() =>state.budget > 0, [state.budget])
-  return (
+ useEffect(() => {
+  localStorage.setItem('budget', state.budget.toString())
+  localStorage.setItem('expenses', JSON.stringify(state.expenses))
+}, [state]) 
+ return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
         <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de Gastos</h1>
@@ -17,7 +22,8 @@ function App() {
       </div>
       {isValidBudget && (
         <main className="max-x-3xl mx-auto py-10">
-         <ExpenseList/> 
+          <FilterByCategory/>
+          <ExpenseList/> 
           <ExpenseModal/>
         </main>
       )}
